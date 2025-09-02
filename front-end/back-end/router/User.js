d66eda826dc2e12efb632bd3baf53add6f4b0885
dhairyajangid <jangiddhairya@gmail.com>
2025-09-02 22:57:31 +0530

    adding the express logic signup

--- front-end/back-end/router/User.js
+++ front-end/back-end/router/User.js
@@ -1,12 +1,26 @@
+import { UserSchemazod } from '../userValidation';
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.post('/signin',(req,res)=>({
+app.post('/signup', async(req,res)=>{
+    const parsed = UserSchemazod.safeparse(req.body)
+    if(!parsed.success){
+        return res.status(400).json("error user not exist")
+    }
+    const Userexist = await User.findOne({
+        username: req.body.username
+    });
     
-}))
+    if(Userexist){
+        res.status(411).json({
+            msg: "user already exist"
+        })
+    }
+
+})
 
 app.listen(port,()=>{
     console.log(`the server is running on ${port}`);
