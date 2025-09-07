d94d3fa2c26fa022d4a822105871d10f96aae4f3
dhairyajangid <jangiddhairya@gmail.com>
2025-09-07 23:48:28 +0530

    wrong way to make the jwt middleware few issus in syntex and error

--- front-end/back-end/middleware.js
+++ front-end/back-end/middleware.js
@@ -2,7 +2,7 @@ const jwt = require ('jsonwebtoken')
 const JWT_SECRET = require('./config')
 
 
-    const authToken = (req,res,next)=>{
+export const authToken = (req,res,next)=>{
         const token = req.header.autherization.split(" ")[1];
         if(!token || !token.startswith('barer ')){
             res.status(401).json({
@@ -10,11 +10,13 @@ const JWT_SECRET = require('./config')
             })
         }
         try{
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user_id = decoded.user_id;
-        
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.user = decoded;
+        next();
         }
         catch(err){
-    
+            res.status(411).json({
+                error: "Invalid token"
+            })
         }
-}
+};
