f8a173b76486b31e2d94435905cf58698eda3bb3
dhairyajangid <jangiddhairya@gmail.com>
2025-09-09 20:18:05 +0530

    initializition of imageKitio in the project

---
+++ front-end/back-end/config/imageKit.js
@@ -0,0 +1,7 @@
+const imageKit = require('imagekit');
+const imageUpload = new imageUpload({
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
+})
+module.exports = imageKit;
