e8194d6d2c6b1ec1cdc412e34dbc3883546ea454
Dhairya jangid <121332730+dhairyajangid@users.noreply.github.com>
2026-01-04 10:09:56 +0530

    Add new authentication images to authImages.js

--- front-end/NovelNet/src/data/authImages.js
+++ front-end/NovelNet/src/data/authImages.js
@@ -2,4 +2,7 @@ export const authImages = [
   "https://ik.imagekit.io/Tenkai123/authBackground/signin3.jpg?updatedAt=1764162236302",
   "https://ik.imagekit.io/Tenkai123/authBackground/man-sitting.jpg?updatedAt=1764162236303",
   "https://ik.imagekit.io/Tenkai123/authBackground/siginin.jpg?updatedAt=1764162237333",
+  "https://ik.imagekit.io/Tenkai123/authBackground/anime-house.jpg",
+  "https://ik.imagekit.io/Tenkai123/authBackground/Shop.jpg",
+  "https://ik.imagekit.io/Tenkai123/authBackground/library-anime-room.jpg"
 ];
